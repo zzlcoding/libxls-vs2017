f10acd3263f0ce8c1f58f93c3b2fd8bd0cdc7fae
lizz <lizz@geovis.cn>
2018-10-23 12:51:56 +0800

    vs2017 project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # libxls-vs2017
 libxls for visual studio 2017
+clone from https://github.com/evanmiller/libxls.git and add visual studio 2017 project for windows.
---
+++ config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+#define PACKAGE_NAME "libxls"
+#define PACKAGE_TARNAME "libxls"
+#define PACKAGE_VERSION "1.4.0"
+#define PACKAGE_STRING "libxls 1.4.0"
+#define PACKAGE_BUGREPORT "emmiller@gmail.com"
+#define PACKAGE_URL ""
+#define LIBXLS_MAJOR_VERSION 1
+#define LIBXLS_MINOR_VERSION 4
+#define LIBXLS_MICRO_VERSION 0
+#define PACKAGE "libxls"
+#define VERSION "1.4.0"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define LT_OBJDIR ".libs/"
+#define HAVE_STRDUP 1
+#define HAVE_WCHAR_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_MALLOC 1
+#define HAVE_STDLIB_H 1
+#define HAVE_REALLOC 1
+#define HAVE_ICONV 1
